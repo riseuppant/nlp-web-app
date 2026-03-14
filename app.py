db54df540b2b81dd7f418207b4b14b5b0ba345dc
hardikbatwal1505@gmail.com <hardikbatwal1505@gmail.com>
2026-03-15 00:47:27 +0530

    Added Post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -8,5 +8,12 @@ def index():
 @app.route('/register')
 def register():
     return render_template('register.html')
+@app.route('/perform_registration',methods=['POST'])
+def perform_registration():
+    # return "Something"
+    name = request.form.get('user_name')
+    email = request.form.get('user_email')
+    password = request.form.get('user_password')
+    return name + " "+ email + " "+ password
 
 app.run(debug=True)
