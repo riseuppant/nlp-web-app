9d1824c38c366356328ecfb40ebf5d560fe56384
hardikbatwal1505@gmail.com <hardikbatwal1505@gmail.com>
2026-03-15 14:12:40 +0530

    NER files added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
+from db import Database
+
+dbo = Database()
 
 app = Flask(__name__)
 
@@ -14,6 +17,32 @@ def perform_registration():
     name = request.form.get('user_name')
     email = request.form.get('user_email')
     password = request.form.get('user_password')
-    return name + " "+ email + " "+ password
+    
+    response = dbo.insert(name,email,password)
 
+    if response:
+        return render_template("login.html",message="Registration Successful Now Login to Proceed")
+    else:
+        return render_template("register.html",message ="Account already exists")
+
+@app.route('/perform_login',methods=['POST'])
+def perform_login():
+
+    email = request.form.get('user_email')
+    password = request.form.get('user_password')
+    
+    response = dbo.search(email,password)
+
+    if response:
+        return redirect('/profile')
+    else:
+        return render_template('login.html',message='incorrect email/password')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+@app.route('/ner')
+def ner():
+    return render_template('ner.html')
+@app.route('/perform_ner')
 app.run(debug=True)
